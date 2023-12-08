c44645a2c9b243a6d93407974fb602d97b5d231a
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-12-08 16:54:33 +0200

    add first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function () {
+
+
+    // var advantages = $('.advantages');
+    // var background = $('.background');
+
+    // function updateStyles() {
+    //     var scrollPos = $(window).scrollTop();
+    //     var elementOffset = advantages.height();
+    //     var windowHeight = $(window).height();
+    //     var start = scrollPos - elementOffset - 400
+        
+    //     console.log(scrollPos);
+    //     console.log(elementOffset);
+    //     console.log(start);
+
+    //     if (scrollPos > elementOffset + 400) {
+
+    //         var rotateValue = 0.05 * (start * 0.1);
+    //         background.css({
+    //             'transform': 'rotate(' + rotateValue + 'deg)'
+    //         });
+
+    //     } else {
+    //         background.css({
+    //             'transform': 'rotate(' + 1 + 'deg)'
+    //         });
+    //     }
+    // }
+    // $(window).scroll(updateStyles);
+
+    // $(document).ready(updateStyles);
+
+
+    // --- accordeon --- //
+    $('.faq__header').on('click', function() {
+        $this = $(this);
+        $this.toggleClass('active');
+        $this.next('.faq__body').slideToggle();
+    });
+
+
+    // --- smooth scroll --- //
+    // const locoScroll = new LocomotiveScroll({
+    //     el: document.querySelector(".smooth-scroll"),
+    //     smooth: true
+    // });
+
+});
