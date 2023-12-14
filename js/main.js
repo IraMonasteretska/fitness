bb299b620dfd900fc4dd34f0af448a7768ff5882
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-12-14 10:46:18 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        speed:900,
+        speed: 900,
         autoplaySpeed: 1880,
         infinite: true,
         arrows: false,
@@ -44,74 +44,20 @@ $(document).ready(function () {
     });
 
 
-
-
-    // var advantages = $('.advantages');
-    // var background = $('.background');
-
-    // function updateStyles() {
-    //     var scrollPos = $(window).scrollTop();
-    //     var elementOffset = advantages.height();
-    //     var windowHeight = $(window).height();
-    //     var start = scrollPos - elementOffset - 400
-
-    //     console.log(scrollPos);
-    //     console.log(elementOffset);
-    //     console.log(start);
-
-    //     if (scrollPos > elementOffset + 400) {
-
-    //         var rotateValue = 0.05 * (start * 0.1);
-    //         background.css({
-    //             'transform': 'rotate(' + rotateValue + 'deg)'
-    //         });
-
-    //     } else {
-    //         background.css({
-    //             'transform': 'rotate(' + 1 + 'deg)'
-    //         });
-    //     }
-    // }
-    // $(window).scroll(updateStyles);
-
-    // $(document).ready(updateStyles);
+    // player
+    if ($('#player').length) {
+        const player = new Plyr("#player");
+    }
 
 
 });
 
-if (document.querySelector('video')) {
-    // const player = new Plyr('#player');
-    document.addEventListener('DOMContentLoaded', () => {
-        // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
-        const controls = [
-            'play-large', // The large play button in the center
-            'rewind', // Rewind by the seek time (default 10 seconds)
-            'play', // Play/pause playback
-            'fast-forward', // Fast forward by the seek time (default 10 seconds)
-            'progress', // The progress bar and scrubber for playback and buffering
-            'current-time', // The current time of playback
-            'duration', // The full duration of the media
-            'mute', // Toggle mute
-            'volume', // Volume control
-            'captions', // Toggle captions
-            'settings', // Settings menu
-            'pip', // Picture-in-picture (currently Safari only)
-            'airplay', // Airplay (currently Safari only)
-            'fullscreen' // Toggle fullscreen
-        ];
 
-        // const player = new Plyr('#player', { controls });
-        const players = Plyr.setup('.video__controls', {
-            controls,
-            autoplay: 'false',
-        });
 
-        // Expose
-        window.players = players;
 
-        // Bind event listener
-        function on(selector, type, callback) {
-            document.querySelector(selector).addEventListener(type, callback, false);
-        }
-    });
-}
+AOS.init({
+    duration: 1400,
+    disable: 'mobile',
+    easing: 'ease',
+    once: true,
+});
